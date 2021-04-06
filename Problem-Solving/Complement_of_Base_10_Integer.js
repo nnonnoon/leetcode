d6d1393988ed22ba1a4d4e7abd76a12cc7927a7a
Jarawee THONGCHAROEN <jarawee.t@ku.th>
2021-04-06 17:55:29 +0700

    [+] Complement_of_Base_10_Integer

---
+++ Problem-Solving/Complement_of_Base_10_Integer.js
@@ -0,0 +1,44 @@
+let N = 67185;
+
+// const result = N.toString(2)
+
+// console.log(result)
+
+let bin = 0;
+let rem, i = 1 ;
+
+while(N !== 0){
+    rem = N % 2
+    N = Math.floor(N / 2)
+
+    bin += rem * i;
+    i = i* 10;
+}
+
+console.log(bin)
+
+
+bin = String(bin)
+new_bin = bin.split("")
+
+let res = "";
+let final = 0;
+let j = 0;
+
+for(let i = new_bin.length - 1  ; i > -1 ; i--){
+    if(new_bin[i] === "1"){
+        res = "0" + res;
+        final += 0 * (2**j);
+    }
+    else if (new_bin[i] === "0"){
+        res = "1" + res;
+        final += 1 * (2**j);
+    }
+    j += 1;
+}
+
+console.log(res)
+console.log(final)
+
+
+
--- Problem-Solving/test.js
+++ Problem-Solving/test.js
@@ -1,14 +1,35 @@
-let arr = [1, 2, 3, 4, 5];
+// let arr = [1, 2, 3, 4, 5];
 
-// arr = arr.map(index => index * 5);
+// // arr = arr.map(index => index * 5);
 
-// arr = arr.filter(index => index !== '5')
+// // arr = arr.filter(index => index !== '5')
 
-// arr = arr.reduce((acc, curr) => {
-//     return acc + curr
-// }, 0);
+// // arr = arr.reduce((acc, curr) => {
+// //     return acc + curr
+// // }, 0);
 
-arr.forEach((element, index) =>  {return arr[index] =  element * 2});
+// arr.forEach((element, index) =>  {return arr[index] =  element * 2});
 
-console.log(arr)
+// console.log(arr)
+
+let digit = "12345";
+
+arr = digit.split("")
+
+let sum = 0;
+
+sum = arr.reduce((acc, curr) => {return acc + Number(curr)},0)
+
+console.log(sum)
+
+let digit2 = 12345;
+
+let sum2 = 0;
+
+while (digit2 !== 0){
+    sum2 += digit2 % 10;
+    digit2 = Math.floor(digit2/10);
+}
+
+console.log(sum2)
 
